957b4a9ffe5634a2c4f281e6d752d60eaf2d235f
antoniolago <45375617+antoniolago@users.noreply.github.com>
2024-07-22 05:02:30 -0300

    tst

--- api/entrypoint_deploy.sh
+++ api/entrypoint_deploy.sh
@@ -6,7 +6,7 @@ git clone https://github.com/adafruit/Adafruit_Python_DHT
 # Build and install the Adafruit Python DHT library
 cd Adafruit_Python_DHT
 python setup.py build
-python setup.py install
+python setup.py install --force-pi
 
 # Run the main Python script
 python /api/main.py
